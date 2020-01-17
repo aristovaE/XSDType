ed90e1800a657eb0fa94528b5e350cf0726e08bc
aristovaE <59869569+aristovaE@users.noreply.github.com>
2020-01-17 17:02:57 +0300

    Add files via upload
    1)считывает элементы в dataGridView(имя+тип) (только тег <xsd:all>)
    2)все элементы в treeView (без названий имен)

---
+++ XSDType4.2/XSDType4/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace XSDType4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "*.XSD READ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(534, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(234, 339);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "element";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "type";
+            this.Column2.Name = "Column2";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 73);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(262, 339);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 428);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ XSDType4.2/XSDType4/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace XSDType4
+{
+    //не выводит имена, выводит все элементы
+    public partial class Form1 : Form
+    {
+        XmlSchemaSet xss = null;
+        XmlSchema xs = null;
+        XmlSchemas schemas = null;
+
+        ValidationEventHandler ValidationErrorHandler = null;
+        public Form1()
+        {
+            InitializeComponent();
+            var doc = XDocument.Load(@"...\..\..\..\xsd\Passport.xsd");
+            var root = doc.Root;
+            var x = GetNodes(new TreeNode(root.Name.LocalName), root).ToArray();
+
+            treeView1.Nodes.AddRange(x);
+
+
+        }
+
+        private IEnumerable<TreeNode> GetNodes(TreeNode node, XElement element)
+        {
+            return element.HasElements ?
+                node.AddRange(from item in element.Elements()
+                              let tree = new TreeNode(item.Name.LocalName)
+                              from newNode in GetNodes(tree, item)
+                              select newNode)
+                              :
+                new[] { node };
+        }
+
+        public void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            XmlNodeList nodeList;
+            try
+            {
+                if (inXmlNode.HasChildNodes)
+                {
+                    
+                        
+                    nodeList = inXmlNode.ChildNodes;
+                    
+                    for (int i = 0; i < nodeList.Count - 1; i++)
+                    {
+                        xNode = inXmlNode.ChildNodes[i];
+                        //добавляем новый узел в дерево
+                        if (nodeList[i].NodeType == XmlNodeType.Element)
+                        {
+                            inTreeNode.Nodes.Add(new TreeNode(xNode.LocalName));
+                        }
+                        else
+                        {
+                            //добавляем значение элемента
+                            if (nodeList[i].NodeType == XmlNodeType.Text)
+                            {
+                                inTreeNode.NextNode.Nodes.Add(new TreeNode("Value",
+                                new TreeNode[] { new TreeNode(inXmlNode.InnerText) }));
+                            }
+                        }
+                        //добавляем атрибуты
+                        if (xNode.Attributes != null)
+                        {
+                            foreach (XmlAttribute atr in xNode.Attributes)
+                            {
+                                inTreeNode.LastNode.Nodes.Add(new TreeNode(atr.Name,
+                                new TreeNode[] { new TreeNode(atr.Value) }));
+                            }
+                        }
+                        //рекурсия
+                        AddNode(xNode, inTreeNode.Nodes[i]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo diXsd = new DirectoryInfo(Path.Combine(Application.StartupPath, @"..\..\..\..\xsd\"));
+
+
+
+            xss = new XmlSchemaSet();
+            xss.ValidationEventHandler += ValidationErrorHandler;
+            xss.XmlResolver = new XmlUrlResolver();
+
+            schemas = new XmlSchemas();
+            foreach (var fi in diXsd.GetFiles("Passport.xsd")) //Passport.xsd читается полностью
+                                                               //разбиение на функции + цикличность?
+                                                               //datagridview
+            {
+                using (var sr = new StreamReader(fi.FullName))
+                {
+                    xs = XmlSchema.Read(sr, ValidationErrorHandler);
+                    xss.Add(xs);
+                    schemas.Add(xs);
+                }
+                MessageBox.Show("Schema " + fi.Name + " read successully ");
+
+            }
+
+            xss.Compile();
+
+            XmlSchemaElement schemaElement = null;
+
+            foreach (XmlSchema schema in xss.Schemas())
+            {
+                foreach (var sChemaItem in schema.Items)
+                {
+                    schemaElement = sChemaItem as XmlSchemaElement;
+                    if (schemaElement != null)
+                    {
+
+
+                        MessageBox.Show("ELEMENT: " + schemaElement.Name);
+
+                        // Get the complex type of the element.
+                        XmlSchemaComplexType complexType = schemaElement.ElementSchemaType as XmlSchemaComplexType;
+                        MessageBox.Show("NAME OF COMPLEX TYPE OF " + schemaElement.Name + " = " + complexType.Name);
+
+
+                        // Get the sequence particle of the complex type.
+                        XmlSchemaSequence sequence = complexType.ContentTypeParticle as XmlSchemaSequence;
+                        int i = 0;
+                        // Iterate over each XmlSchemaElement in the Items collection.
+                        foreach (XmlSchemaElement childElement in sequence.Items)
+                        {
+                            //  MessageBox.Show("ELEMENT OF "+ complexType.Name+" = " + childElement.Name);
+
+
+
+
+
+                            XmlSchemaComplexType complexType2 = childElement.ElementSchemaType as XmlSchemaComplexType;
+                            if (childElement.ElementSchemaType.Name != null)
+                            {
+                                MessageBox.Show("COMPLEX TYPE OF " + childElement.Name + " = " + childElement.ElementSchemaType.Name);
+                            }
+
+
+                            //  MessageBox.Show("NAME OF COMPLEX TYPE OF " + childElement.Name + " = " + complexType2.Name);
+
+
+                            // DataGridView dataGridView1 = new DataGridView();//Create new grid
+                            //dataGridView1.Columns[0].Name = "element";
+                            dataGridView1.ColumnCount = 2;
+                            // Get the sequence particle of the complex type.
+
+                            XmlSchemaAll all = complexType2.Particle as XmlSchemaAll; // <xsd:all>
+                            if (all != null)
+                            {
+                                dataGridView1.RowCount = all.Items.Count;
+
+                                // Iterate over each XmlSchemaElement in the Items collection.
+                                foreach (XmlSchemaElement childElement2 in all.Items)
+                                {
+                                    if (complexType2.Name != null)
+                                    {
+                                        MessageBox.Show("ELEMENT OF " + complexType2.Name + " = " + childElement2.Name);
+                                        dataGridView1.Rows[i].Cells[0].Value = childElement2.Name;
+                                        dataGridView1.Rows[i].Cells[1].Value = complexType2.Name;
+                                    }
+                                    else
+                                    {// MessageBox.Show("ELEMENT OF COMPLEX TYPE = " + childElement2.Name);
+                                    }
+
+                                    dataGridView1.Rows[i].Cells[0].Value = childElement2.Name;
+                                    if (childElement2.ElementSchemaType.TypeCode.ToString() != "None")
+                                        // MessageBox.Show("TYPE OF " + childElement2.Name + " = " + childElement2.ElementSchemaType.TypeCode);
+                                        dataGridView1.Rows[i].Cells[1].Value = childElement2.ElementSchemaType.TypeCode;
+                                    else dataGridView1.Rows[i].Cells[1].Value = childElement2.ElementSchemaType.Name;
+
+                                    i++;
+
+                                }
+                                if (complexType2.AttributeUses.Count > 0)
+                                {
+                                    IDictionaryEnumerator enumerator =
+                                        complexType2.AttributeUses.GetEnumerator();
+
+                                    while (enumerator.MoveNext())
+                                    {
+                                        XmlSchemaAttribute attribute =
+                                            (XmlSchemaAttribute)enumerator.Value;
+
+                                        MessageBox.Show("Attribute: " + attribute.Name);
+                                    }
+                                }
+
+                            }
+
+                        }
+
+
+
+
+                    }
+                }
+            }
+
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            MessageBox.Show(e.Node.Text);
+        }
+    }
+    public static class TreeNodeEx
+    {
+        public static IEnumerable<TreeNode> AddRange(this TreeNode collection, IEnumerable<TreeNode> nodes)
+        {
+            var items = nodes.ToArray();
+            collection.Nodes.AddRange(items);
+            return new[] { collection };
+        }
+    }
+}
