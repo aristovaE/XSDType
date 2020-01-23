bb52d3d44e44a28de335272ff97b82cd07ed4296
aristovaE <59869569+aristovaE@users.noreply.github.com>
2020-01-23 12:37:12 +0300

    чтение первых элементов xsd -> класс Schema
    1) отдельная функция для чтения xsd файла
    2) отдельная функция для чтения первых корневых(?) элементов и занесения их в класс
    3) отдельная функция для записи класса в treeView

--- XSDTypeCl/XSDTypeCl/Form1.cs
+++ XSDTypeCl/XSDTypeCl/Form1.cs
@@ -17,87 +17,31 @@ namespace XSDTypeCl
 {
     public partial class Form1 : Form
     {
+        XmlSchemaSet xss = null;
+        XmlSchema xs = null;
+        XmlSchemas schemas = null;
+        ValidationEventHandler ValidationErrorHandler = null;
+
+        XmlDocument doc = new XmlDocument();
+        static string xsdName = "AirPassList.xsd";
+        string filePath = @"...\..\..\..\xsd\" + xsdName;
+
+        DirectoryInfo diXsd = new DirectoryInfo(Path.Combine(Application.StartupPath, @"..\..\..\..\xsd\"));
+
+
         public Form1()
         {
             InitializeComponent();
         }
-
-
-
-        private void treeViewBtn_Click(object sender, EventArgs e)
+        
+        public void ReadXSD()
         {
-            Schema Passport;
-            SchemaItem PassportTable;
-            List<SchemaItem> PassportItem;
-
-            List<SchemaItem> PassportItem2;
-            Passport = new Schema("Passport", "Паспорт сделки", PassportTable = new SchemaItem("PassportTable", PassportItem = new List<SchemaItem>()));
-            PassportItem.Add(new SchemaItem("PassportItem", PassportItem2 = new List<SchemaItem>()));
-            PassportItem2.Add(new SchemaItem("DOC_ID", "int"));
-            PassportItem2.Add(new SchemaItem("RegNum", "string", "Номер паспорта"));
-            PassportItem2.Add(new SchemaItem("RegDate", "dateTime", "Дата паспорта"));
-
-            // Clear the TreeView each time the method is called.
-            treeView1.Nodes.Clear();
-
-            treeView1.Nodes.Add(new TreeNode(Passport.name));
-            treeView1.Nodes[0].Nodes.Add(new TreeNode(Passport.discription));
-            treeView1.Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.name));
-            treeView1.Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].name));
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].name));
-
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].type));
-
-
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].name));
-
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].type));
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].discription));
-
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].name));
-
-
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].type));
-            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].discription));
-
-            MessageBox.Show("Вручную");
-            treeView1.ExpandAll();
-        }
-
-        private void xsdToTreeViewBtn_Click(object sender, EventArgs e)
-        {
-            Schema Passport;
-            SchemaItem PassportTable;
-            List<SchemaItem> PassportItem;
-
-            List<SchemaItem> PassportItem2;
-            //Passport = new Schema("Passport", "Паспорт сделки", PassportTable = new SchemaItem("PassportTable", PassportItem = new List<SchemaItem>()));
-            //PassportItem.Add(new SchemaItem("PassportItem", PassportItem2 = new List<SchemaItem>()));
-            //PassportItem2.Add(new SchemaItem("DOC_ID", "int"));
-            //PassportItem2.Add(new SchemaItem("RegNum", "string", "Номер паспорта"));
-            //PassportItem2.Add(new SchemaItem("RegDate", "dateTime", "Дата паспорта"));
-
             treeView2.Nodes.Clear();
-            XmlSchemaSet xss = null;
-            XmlSchema xs = null;
-            XmlSchemas schemas = null;
-            ValidationEventHandler ValidationErrorHandler = null;
-
-            XmlDocument doc = new XmlDocument();
-            string xsdName = "Passport.xsd";
-            string filePath = @"...\..\..\..\xsd\"+xsdName;
-
-            DirectoryInfo diXsd = new DirectoryInfo(Path.Combine(Application.StartupPath, @"..\..\..\..\xsd\"));
-
             xss = new XmlSchemaSet();
             xss.ValidationEventHandler += ValidationErrorHandler;
             xss.XmlResolver = new XmlUrlResolver();
-            int i = 0;
-            int y = 0;
             schemas = new XmlSchemas();
-            foreach (var fi in diXsd.GetFiles(xsdName)) //Passport.xsd читается полностью
-                                                               //разбиение на функции + цикличность?
-                                                               //datagridview
+            foreach (var fi in diXsd.GetFiles(xsdName))
             {
                 using (var sr = new StreamReader(fi.FullName))
                 {
@@ -109,113 +53,147 @@ namespace XSDTypeCl
 
             }
             xss.Compile();
+        }
+        public void ClassToTreeView(Schema Passport, List<SchemaItem> PassportItem)
+        {
 
-            XmlSchemaElement schemaElement = null;
-
-
-            foreach (var sChemaItem in xs.Items)
-            {
-                schemaElement = sChemaItem as XmlSchemaElement;
-                if (schemaElement != null)
-                {
-
-                   // treeView2.Nodes.Add(schemaElement.Name + " (Root)");
-                   // ((System.Xml.Schema.XmlSchemaType)new System.Linq.SystemCore_EnumerableDebugView(((System.Xml.Schema.XmlSchema)schemaElement.Parent).Items).Items[2]).Name
-                    XmlSchemaComplexType complexType = schemaElement.ElementSchemaType as XmlSchemaComplexType;
-
-                    Passport = new Schema(schemaElement.Name, PassportTable = new SchemaItem(complexType.Name, PassportItem = new List<SchemaItem>()));
-                    treeView2.Nodes.Add(new TreeNode(Passport.name + " (Root)"));
-                    //treeView2.Nodes[0].Nodes.Add(complexType.Name + " (ComplexType)");
-                    treeView2.Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.name + " (ComplexType)"));
-                    XmlSchemaSequence sequence = complexType.ContentTypeParticle as XmlSchemaSequence;
-                    
-                    foreach (XmlSchemaElement childElement in sequence.Items)
-                    {
-                        PassportItem.Add(new SchemaItem(childElement.Name, PassportItem2 = new List<SchemaItem>()));
-                      //  treeView2.Nodes[0].Nodes[i].Nodes.Add(childElement.Name + " (element)");
-
-                        treeView2.Nodes[0].Nodes[i].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].name + " (element)"));
-                        XmlSchemaComplexType complexType2 = childElement.ElementSchemaType as XmlSchemaComplexType;
-                        if (childElement.ElementSchemaType.Name != null)
-                        {
-
-                           // treeView2.Nodes[0].Nodes[i].Nodes[0].Nodes.Add(childElement.ElementSchemaType.Name);
-                          //  treeView2.Nodes[0].Nodes[i].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].name));
-                           
-
-                        }
-
-                        XmlSchemaAll all = complexType2.Particle as XmlSchemaAll; // <xsd:all>
-                        if (all != null)
-                        {
-                            
-
-                            foreach (XmlSchemaElement childElement2 in all.Items)
-                            {
-                                
-                                if (complexType2.Name != null)
-                                {
-
-                                }
-                                else
-                                {
-                                    if (childElement2.ElementSchemaType.TypeCode.ToString() != "None")
-                                    {
-                                        PassportItem2.Add(new SchemaItem(childElement2.Name, childElement2.ElementSchemaType.TypeCode.ToString()));
-                                        //treeView2.Nodes[0].Nodes[i].Nodes[0].Nodes.Add(childElement2.Name + " (" + childElement2.ElementSchemaType.TypeCode + ")");
-                                        treeView2.Nodes[0].Nodes[i].Nodes[0].Nodes.Add(Passport.schemaItem.schemaItems[0].schemaItems[y].name+" ("+Passport.schemaItem.schemaItems[0].schemaItems[y].type+")");
-                                    }
-                                }
-
-                                y++;
-                            }
-                            if (complexType2.AttributeUses.Count > 0)
-                            {
-                                IDictionaryEnumerator enumerator =
-                                    complexType2.AttributeUses.GetEnumerator();
-
-                                while (enumerator.MoveNext())
-                                {
-                                    XmlSchemaAttribute attribute =
-                                        (XmlSchemaAttribute)enumerator.Value;
-
-                                    MessageBox.Show("Attribute: " + attribute.Name);
-                                }
-                            }
-
-                        }
-
-                    }
-
-
-
-
-                }
-            }
-
-
-           
-            //treeView2.Nodes[0].Nodes.Add(new TreeNode(Passport.discription));
+            treeView2.Nodes.Add(Passport.name);
+            foreach (SchemaItem pI in PassportItem)
+            { treeView2.Nodes[0].Nodes.Add(pI.name); }
             //treeView2.Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.name));
             //treeView2.Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].name));
             //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].name));
-
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].type));
-
-
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].name));
-
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].type));
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].discription));
-
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].name));
-
-
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].type));
-            //treeView2.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].discription));
+        }
+        public void RecursionForSchemaElement(Schema Passport,XmlSchemaElement schemaElement)
+        {
+            //XmlSchemaComplexType complexType = schemaElement.ElementSchemaType as XmlSchemaComplexType;
             
+            ////treeView2.Nodes.Add(new TreeNode(Passport.name + " (Root)"));
+            ////treeView2.Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.name + " (ComplexType)"));
+            //XmlSchemaSequence sequence = complexType.ContentTypeParticle as XmlSchemaSequence;
+
+            //Passport.ReadXSD(schemaElement);
+        }
+
+        public void RecursionForSchemaComplexType()
+        {
+
+        }
+        private void xsdToTreeViewBtn_Click(object sender, EventArgs e)
+        {
+            ReadXSD();
+            Schema Passport;
+            SchemaItem PassportTable=new SchemaItem("","","");
+            List<SchemaItem> PassportItem;
+            List<SchemaItem> PassportItemInCT;
+            Passport = new Schema(xsdName, PassportItem=new List<SchemaItem>()) ;
+
+            XmlSchemaElement schemaElement = null;
+            XmlSchemaComplexType schemaType = null;
+
+            int i = 0;
+            int y = 0;
+
+            foreach (var sChemaItem in xs.Items)
+            {
+
+                //schemaElement = sChemaItem as XmlSchemaElement;
+                //schemaType = sChemaItem as XmlSchemaComplexType;
+                if (sChemaItem is XmlSchemaElement)
+                {
+                    //RecursionForSchemaElement(Passport, schemaElement);
+                    schemaElement = sChemaItem as XmlSchemaElement;
+                    PassportItem.Add(new SchemaItem(schemaElement.Name, schemaElement.SchemaTypeName.ToString()));
+
+                }
+                else if (sChemaItem is XmlSchemaComplexType)
+                {
+                    schemaType = sChemaItem as XmlSchemaComplexType;
+                    PassportItem.Add(new SchemaItem(schemaType.Name, PassportItemInCT = new List<SchemaItem>()));
+
+                }
+                
+            }
+            //foreach (SchemaItem pI in PassportItem)
+            //{ MessageBox.Show(pI.name + " " + pI.discription + " " + pI.type); }
+            //    foreach (XmlSchemaElement childElement in sequence.Items)
+            //    {
+            //        PassportItem.Add(new SchemaItem(childElement.Name, PassportItem2 = new List<SchemaItem>()));
+
+            //        treeView2.Nodes[0].Nodes[i].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].name + " (element)"));
+            //        XmlSchemaComplexType complexType2 = childElement.ElementSchemaType as XmlSchemaComplexType;
+            //        if (childElement.ElementSchemaType.Name != null)
+            //        {
+
+            //        }
+
+            //        XmlSchemaAll all = complexType2.Particle as XmlSchemaAll; // <xsd:all>
+            //        if (all != null)
+            //        {
+
+
+            //            foreach (XmlSchemaElement childElement2 in all.Items)
+            //            {
+
+            //                if (complexType2.Name != null)
+            //                {
+
+            //                }
+            //                else
+            //                {
+
+            //                        PassportItem2.Add(new SchemaItem(childElement2.Name, childElement2.ElementSchemaType.TypeCode.ToString()));
+            //                        treeView2.Nodes[0].Nodes[i].Nodes[0].Nodes.Add(Passport.schemaItem.schemaItems[0].schemaItems[y].name+" ("+Passport.schemaItem.schemaItems[0].schemaItems[y].type+")");
+
+            //                }
+
+            //                y++;
+            //            }
+            //            if (complexType2.AttributeUses.Count > 0)
+            //            {
+            //                IDictionaryEnumerator enumerator =
+            //                    complexType2.AttributeUses.GetEnumerator();
+
+            //                while (enumerator.MoveNext())
+            //                {
+            //                    XmlSchemaAttribute attribute =
+            //                        (XmlSchemaAttribute)enumerator.Value;
+
+            //                    MessageBox.Show("Attribute: " + attribute.Name);
+            //                }
+            //            }
+
+            //        }
+
+            ClassToTreeView(Passport, PassportItem);
             treeView2.ExpandAll();
 
         }
+
+        //treeView1.Nodes.Clear();
+
+        //treeView1.Nodes.Add(new TreeNode(Passport.name));
+        //treeView1.Nodes[0].Nodes.Add(new TreeNode(Passport.discription));
+        //treeView1.Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.name));
+        //treeView1.Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].name));
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].name));
+
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[0].type));
+
+
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].name));
+
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].type));
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[1].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[1].discription));
+
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].name));
+
+
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].type));
+        //treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes[2].Nodes.Add(new TreeNode(Passport.schemaItem.schemaItems[0].schemaItems[2].discription));
+
+
+
     }
 }
+
